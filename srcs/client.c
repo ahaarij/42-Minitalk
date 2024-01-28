db1fd0512e59a8577c0f48ee307f72da49c9b70e
Ahmed Aarij <ahaarij@lab1r3s9.42abudhabi.ae>
2024-01-28 18:34:25 +0400

    PUSH

--- srcs/client.c
+++ srcs/client.c
@@ -6,25 +6,25 @@
 /*   By: ahaarij <ahaarij@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 13:25:29 by ahaarij           #+#    #+#             */
-/*   Updated: 2024/01/23 20:41:11 by ahaarij          ###   ########.fr       */
+/*   Updated: 2024/01/28 13:53:53 by ahaarij          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-static void	send_msg(int pid, char msg)
+static void	send_msg(int pid, char msg)	//deconstructs characters into bits and sends it using kill
 {
 	int	bit;
 
-	bit = 7;
+	bit = 7;					// starts with most significant bit which is the first bit (leftmost)
 	while (bit >= 0)
 	{
-		if(msg >> bit & 1)
-			kill(pid, SIGUSR1);
+		if(msg >> bit & 1)		// checks if the current bit is 1 ( & 1 is the same as == 1 ) after shifting to the right 'bit' amount of times.
+			kill(pid, SIGUSR1);	// kill takes in a process ID and signal. it send the specified signal to the specified process id. in this case it will send to server.
 		else
 			kill(pid, SIGUSR2);
-		usleep(800);
-		bit--;
+		usleep(400);			// 0.8ms before processing next bit. controls rate of signal transmission.
+		bit--;					// keeps doing this until bit reaches 0 aka character completed
 	}
 }
 
@@ -51,12 +51,12 @@ int	checkinput(int argc, char **argv)
 	int	correct_input;
 
 	correct_input = 0;
-	if (argc != 3)
+	if (argv[2] == 0)
+		ft_printf("%s\n", "Please input a non-empty message!");
+	else if (argc != 3)
 		ft_printf("%s\n", "Please use the correct format! ./client <PID> <Message>");
 	else if (!isstrdig(argv[1]))
 		ft_printf("%s\n", "Wrong Format!, PID should only contain numbers!");
-	else if (argv[2] == 0)
-		ft_printf("%s\n", "Please input a non-empty message!");
 	else
 		correct_input = 1;
 	return (correct_input);
@@ -83,3 +83,20 @@ int	main(int argc, char **argv)
 	}
 	return (0);
 }
+
+// note to keep in mind
+// ascii is converted to binary like this
+// lets say we're converting B to binary
+// ascii for B is 66
+// so we start dividing by 2. if there is a remainder, it is set to 1. and if there isnt a remainder, it is set to 0 as such:
+// 66 / 2 = 33 (0) (Least significant bit, so this will be on the right side)
+// 33 / 2 = 16.5 (1)
+// 16 / 2 = 8 (0)
+// 8 / 2 = 4 (0)
+// 4 / 2 = 2 (0)
+// 2 / 2 = 1 (0)
+// 1 / 2 = 0.5 (1) (Most significant bit, so this will be on the left side)
+// the way we put this into binary is:
+// Most significant on the left and everything in between until Least significant bit is on the right
+// so the binary conversion would be
+// 1000010
--- srcs/server.c
+++ srcs/server.c
@@ -6,23 +6,23 @@
 /*   By: ahaarij <ahaarij@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 12:50:15 by ahaarij           #+#    #+#             */
-/*   Updated: 2024/01/23 14:12:23 by ahaarij          ###   ########.fr       */
+/*   Updated: 2024/01/28 13:49:08 by ahaarij          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-static void	bits(int sig)
+static void	bits(int sig) // reconstructs the character from bits
 {
 	static int	bit;
 	static int	set;
 
-	if (sig == SIGUSR1)
-		set += 1 << (7 - bit);
+	if (sig == SIGUSR1)			// if bit is 1
+		set += 1 << (7 - bit);	// sets the bit to 1				100001
 	bit++;
-	if (bit == 8)
+	if (bit == 8)				// once bits form a byte
 	{
-		ft_printf("%c", set);
+		ft_printf("%c", set);	// prints out the character formed
 		bit = 0;
 		set = 0;
 	}
@@ -36,12 +36,12 @@ int	main(int argc, char **argv)
 	serv_pid = getpid();
 	if (argc != 1)
 	{
-		ft_printf("%s\n", "ur input is wrong loserrr: ./server");
+		ft_printf("%s\n", "Incorrect input!\nUse: ./server");
 		return (0);
 	}
 	ft_printf("%s%d\n", "This is the Process ID:", serv_pid);
-	signal(SIGUSR1, bits);
-	signal(SIGUSR2, bits);
+	signal(SIGUSR1, bits);												// this is a signal handler, when it recieves SIGUSR1 signal, it will execute the function bits with SIGUSR1 as an argument
+	signal(SIGUSR2, bits);												// this is a signal handler, when it recieves SIGUSR2 signal, it will execute the function bits with SIGUSR2 as an argument
 	while (1)
 		pause();
 	return (0);
